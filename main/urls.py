2f05a529b7e48af478053687deecd0766ae96307
muzkii <andriyo.averill@ui.ac.id>
2024-10-09 11:05:08 +0700

    Averill git push, adding login and register, changes to base.html and changes to homepage.html

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 # from main.views import show_main
-from main.views import homepage
+from main.views import homepage, register, login_user, logout_user
 
 app_name = 'main'
 
 urlpatterns = [
     # path('', show_main, name='show_main'),
     path('', homepage, name='homepage'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout')
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from main.models import Product
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+import datetime
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('main:homepage')
+    context = {
+        'form': form
+    }
+
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
 
 # def show_main(request):
 #     context = {
@@ -8,8 +57,6 @@ from django.shortcuts import render
 
 #     return render(request, "main.html", context)
 
-def homepage(request):
-    return render(request, 'homepage.html')
 
 
 

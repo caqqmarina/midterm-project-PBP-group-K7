b2a5cf9fca1cca90ced808ca918c45160937967a
muzkii <andriyo.averill@ui.ac.id>
2024-10-27 02:36:52 +0700

    Averill git PUSH changes to the forms.py login register layout and navbar

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Faculty, Canteen, Stall, Product
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 
 class FacultyForm(forms.ModelForm):
     class Meta:
@@ -20,3 +22,22 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'price', 'stall']
+
+class CustomUserCreationForm(UserCreationForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(label='Username', widget=forms.TextInput)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)

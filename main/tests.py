713691a5485d920854bc033c8aec141f870356eb
caqqmarina <chiara.aqmarina@ui.ac.id>
2024-10-27 22:41:50 +0700

    modified testcases, ran tests. 6/6 cases passed

--- main/tests.py
+++ main/tests.py
@@ -1,26 +1,53 @@
 from django.test import TestCase, Client
-from django.utils import timezone
-from .models import ReviewEntry
+from django.urls import reverse
+from .models import Faculty, Canteen
+from django.contrib.auth.models import User
 
-class MainTest(TestCase):
-    def test_main_url_is_exist(self):
-        response = Client().get('')
+class MainViewsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        Faculty.objects.create(name="Engineering")
+        self.canteen = Canteen.objects.create(name="Main Canteen", faculty_id=1)
+
+    def test_homepage_url_is_exist(self):
+        response = self.client.get(reverse('main:homepage'))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'homepage.html')
 
-    def test_main_using_main_template(self):
-        response = Client().get('')
-        self.assertTemplateUsed(response, 'main.html')
+    def test_login_and_register_view(self):
+        response = self.client.get(reverse('main:login_and_register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login_and_register.html')
 
-    def test_nonexistent_page(self):
-        response = Client().get('/skibidi/')
-        self.assertEqual(response.status_code, 404)
+    def test_faculty_view(self):
+        response = self.client.get(reverse('main:faculty'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'faculty.html')
+        self.assertContains(response, "Engineering")
 
-    def test_strong_review_user(self):
-        #now = timezone.now()
-        review = ReviewEntry.objects.create(
-          name="Takor",
-          place="FISIP",
-          #description = "Canteen with the best food and is accessible anywhere.",
-          rating = 5,
-        )
-        self.assertTrue(review.good_rating)
+    def test_canteen_view(self):
+        response = self.client.get(reverse('main:canteen', args=[self.canteen.name]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'canteen.html')
+        self.assertContains(response, self.canteen.name)
+
+    def test_add_faculty_view(self):
+        user = User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+
+        # Non-admin user should be redirected to login
+        response = self.client.get(reverse('main:add_faculty'))
+        self.assertEqual(response.status_code, 302)  # Redirected due to permissions
+
+        # Make user an admin and test again
+        user.is_staff = True
+        user.save()
+        response = self.client.get(reverse('main:add_faculty'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'add_faculty.html')
+
+    def test_show_json(self):
+        response = self.client.get(reverse('main:show_json'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/json')
+

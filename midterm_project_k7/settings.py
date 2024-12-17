3df6059e84656f7da2e687da61f521cf380dc4b7
muzkii <andriyo.averill@ui.ac.id>
2024-12-17 19:35:35 +0700

    Changing database to Supabase

--- midterm_project_k7/settings.py
+++ midterm_project_k7/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'midterm_project_k7.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  
+        'USER': 'postgres.ixcwryikvxgrwuszaayh',  
+        'PASSWORD': 'HpoXFMFOQbcAYaNU',  
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',  
+        'PORT': '6543',  
     }
 }
 
